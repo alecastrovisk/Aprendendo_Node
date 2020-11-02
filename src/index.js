bd25ba0869d2b184166d8b8408e8fc84a6232ffe
Your Name <alexandre.acrsf@gmail.com>
2020-11-02 15:02:51 -0300

    Tentando aprender middlewares

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { response } = require('express');
 const express = require('express');
 
-const {  uuid } = require('uuidv4')
+const {  uuid, isUuid } = require('uuidv4')
 
 const app = express();
 
@@ -10,8 +10,34 @@ app.use(express.json());
 //Query params: paginação e filtro
 //Route params: identificar recursos(atualizar/deletar)
 //Request body: Conteúdo na hora de criar ou editar um recurso(JSON)
+//Middleware: Interromper totalmente a requisição  ou alterar dados da requisição
+
+
 
 const projects = [];
+
+function logRequest(request, response, next) {
+    const { method, url} = request;
+
+    const logLabel = `[${method.toUpperCase()} ${url}]`;
+    console.log(logLabel);
+
+    return next();
+}
+
+function validateProjectId(request, response, next ) {
+    const { id } = request.params;
+
+    if(!isUuid(id)) {
+        return response.status(400).json({error: 'Invalid project id.'});
+    }
+    
+    return next();
+}
+
+app.use(logRequest);
+
+
 
 
 app.get('/projects', (request, response) => {
@@ -61,7 +87,7 @@ app.put('/projects/:id', (request,response) =>{
 
 });
 
-app.delete('/projects/:id', (request,response) =>{
+app.delete('/projects/:id',validateProjectId, (request,response) =>{
     const { id } = request.params;
 
     const projectIndex = projects.findIndex(project => project.id === id);
